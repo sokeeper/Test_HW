bcd5243edcee2a5b89a36f510501f800ef583c24
tanapon <tanapon>
2017-04-22 17:20:29 +0700

    6.ShootSpawn

---
+++ 6.ShootSpawn/Assets/Scrip/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	void Update (){
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+
+			nextFire = Time.time + fireRate;
+			Instantiate (shot , shotSpawn.position, shotSpawn.rotation);
+		}
+
+	}
+
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);	
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+
+
+    }
+}
