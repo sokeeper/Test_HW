a6f197f124da416d1c85cde26de3b1cbb3790ced
tanapon <tanapon>
2017-04-22 17:12:31 +0700

    4.Move

---
+++ 4.Move/Assets/Scrip/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);	
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+
+
+    }
+}
